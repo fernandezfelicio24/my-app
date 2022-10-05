9478705fd30c28738828ccbf106c4b3024b8e776
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-10-05 14:16:41 +0700

    Learning Django #55 - Introduction User Permission Check

--- my_app/views.py
+++ my_app/views.py
@@ -1,13 +1,22 @@
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.decorators import login_required
 def index(request):
 
     context = {
         'page_title' : 'Home'
     }
-    return render(request, 'index.html', context)
+
+    template_name = None
+    if request.user.is_authenticated:
+        # logika untuk user
+        template_name = 'index_user.html'
+    else:
+        # logika untuk anonymous
+        template_name = 'index_anonymous.html'
+
+    return render(request, template_name, context)
 
 def loginView(request):
 
@@ -16,7 +25,13 @@ def loginView(request):
     }
     user = None
 
-    if request.method == 'POST':
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('index')
+        else:
+            return render(request, 'login.html', context)
+
+    if request.method == "POST":
         print(request.POST)
         username_login = request.POST['username']
         password_login = request.POST['password']
@@ -29,9 +44,11 @@ def loginView(request):
             return redirect('index')
         else:
             return redirect('login')
-    return render(request, 'login.html', context)
 
 
+
+
+@login_required
 def logoutView(request):
 
     context = {
